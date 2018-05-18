432080194c31bde080002aab1469b8f288135c89
Vespertino <Vespertino@C8PC23>
2018-05-18 19:55:39 +0200

    Ramificacion2

--- Ejercicio2/src/Ejercicio2.java
+++ Ejercicio2/src/Ejercicio2.java
@@ -4,6 +4,6 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejemplo de git");
+		System.out.println("Esta es la version2 por ramificacion");
 	}
-
 }
